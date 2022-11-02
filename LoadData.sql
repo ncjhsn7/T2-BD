52a2e80c385510cfcf9eca243f4a7d46a8d808bf
Nicolas Pietro <52969249+ncjhsn7@users.noreply.github.com>
2022-11-01 22:09:07 -0300

    Create LoadData.sql

---
+++ LoadData.sql
@@ -0,0 +1,25 @@
+INSERT INTO Artistas VALUES ('50CENT-01', '50 cent');
+INSERT INTO Artistas VALUES ('DuaLipa-01', 'Dua Lipa');
+INSERT INTO Artistas VALUES ('Khalid-01', 'Khalid');
+INSERT INTO Artistas VALUES ('PostMalone-01', 'Post Malone');
+INSERT INTO Artistas VALUES ('Drake-01', 'Drake');
+INSERT INTO Artistas VALUES ('SNPDG-01', 'Snoop Dog');
+INSERT INTO Artistas VALUES ('BTS-01', 'BTS');
+INSERT INTO Artistas VALUES ('CalvinHarris-01', 'Calvin Harris');
+INSERT INTO Artistas VALUES ('Marshmello-01', 'Marshmello');
+INSERT INTO Artistas VALUES ('21Savage-01', '21 Savage');
+INSERT INTO Artistas VALUES ('Quavo-01', 'Quavo');
+INSERT INTO Artistas VALUES ('LiamPayne-01', 'Liam Payne');
+
+
+INSERT INTO Usuarios VALUES ('ncjhsn', 'Nicolas Pietro');
+INSERT INTO Usuarios VALUES ('cassiusito', 'Cassius DelAvila');
+INSERT INTO Usuarios VALUES ('tashandoq', 'Lucas Tashan');
+INSERT INTO Usuarios VALUES ('eduuhagustinho', 'Eduardo Agustinho');
+INSERT INTO Usuarios VALUES ('cdtx', 'Cassio Daitx');
+INSERT INTO Usuarios VALUES ('vogadok', 'Kevin Vogado');
+INSERT INTO Usuarios VALUES ('user#2', 'User#2');
+INSERT INTO Usuarios VALUES ('user#3', 'User#3');
+INSERT INTO Usuarios VALUES ('user#4', 'User#4');
+INSERT INTO Usuarios VALUES ('user#5', 'User#5');
+
